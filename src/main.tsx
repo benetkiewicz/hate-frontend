1c9419c5eaf92fd90e2a38afd5f165ee6f676f25
Piotr Benetkiewicz <pbenetkiewicz@gmail.com>
2024-01-07 14:57:13 +0100

    externalize css

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/home";
 import List from "./pages/list";
 import Post from "./pages/post";
+import './style.css';
 
 const router  = createBrowserRouter([
   {
