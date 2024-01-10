ff0a58e580cebc80a221e680bea83d23cf5ff1ac
Piotr Benetkiewicz <pbenetkiewicz@gmail.com>
2024-01-10 21:35:05 +0100

    style list of posts

--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -1,10 +1,33 @@
 import { Link } from "react-router-dom";
 
 const List = () => {
-    return (<ul>
-        <li><Link to="/hate-frontend/posts/test-error">Test Error</Link></li>
-        <li><Link to="/hate-frontend/posts/hello-world">Hello World!</Link></li>
-    </ul>);
-}
+  return (
+    <section>
+      <article className="blogTitle">
+        <div className="date">2023-12-31 19:50</div>
+        <h2>
+          <Link to="/hate-frontend/posts/test-error">Missing Post</Link>
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqu.
+        </p>
+      </article>
+      <article className="blogTitle">
+        <div className="date">2024-01-10 19:50</div>
+        <h2>
+          <Link to="/hate-frontend/posts/hello-world">
+            React + Typescript + Vite project from scratch
+          </Link>
+        </h2>
+        <p>
+          I hate frontend. There are so many ways of creating new project and
+          each one has its own flaws and hides some important details from you.
+          This post explains how to setup React + Typescript app from scratch.
+        </p>
+      </article>
+    </section>
+  );
+};
 
 export default List;
